62d4de7a5b9d224bfd6dc65f5dd85d055f20f602
fefo <fede_16_98@hotmail.com>
2020-04-27 20:02:51 -0300

    -

--- Ejercicios/Ejercicio2.c
+++ Ejercicios/Ejercicio2.c
@@ -7,14 +7,14 @@ int main(int argc, char **argv)
 {
 
     // Message
-    char message[20];
-    char recieved[20];
+    char message[50];
+    char recieved[50];
     // Initialize the MPI environment. The two arguments to MPI Init are not
     // currently used by MPI implementations, but are there in case future
     // implementations might need the arguments.
-
-    MPI_Status status;
     MPI_Init(NULL, NULL);
+    MPI_Status status;
+
 
     //Get the number of processes
     int world_size;
@@ -23,10 +23,12 @@ int main(int argc, char **argv)
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    printf("get the rank of the process");
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
+    
     MPI_Get_processor_name(processor_name, &name_len);
     printf("Entrando al if");
     if (world_rank == 0)
