1e359c50605dddb6b03d640bfa5c871b1b8b8e92
fefo <fede_16_98@hotmail.com>
2020-04-30 11:46:06 -0300

    modify ex 2

--- Ejercicios/Ejercicio2.c
+++ Ejercicios/Ejercicio2.c
@@ -21,7 +21,6 @@ int main(int argc, char **argv)
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    printf("get the rank of the process \n");
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -30,24 +29,20 @@ int main(int argc, char **argv)
     MPI_Get_processor_name(processor_name, &name_len);
     printf("Entrando al if \n\n");
 
-    // posible implementacion con barriers.
+
+    int partner_Process = (world_rank+1) % world_size;
     if (world_rank == 0)
     {
         strcpy(message, "Hola, soy el proceso 0");
-        MPI_Send(message, strlen(message), MPI_CHAR, world_rank++, 99, MPI_COMM_WORLD);
+        MPI_Send(message, strlen(message), MPI_CHAR, partner_Process, 99, MPI_COMM_WORLD);
         printf("Mensaje del proceso 0 enviado \n\n");
         MPI_Recv(recieved, 20, MPI_CHAR, world_size--, 99, MPI_COMM_WORLD, &status);
         printf("Recibido el mensaje: %s\n", recieved);
     }
-    else if (world_rank > 0 && world_rank < world_size--)
-    {
-        MPI_Recv(recieved, 20, MPI_CHAR, world_rank--, 99, MPI_COMM_WORLD, &status);
-        MPI_Send(recieved, strlen(recieved), MPI_CHAR, world_rank++, 99, MPI_COMM_WORLD);
-    }
     else
     {
-        MPI_Recv(recieved, 20, MPI_CHAR, world_rank-2, 99, MPI_COMM_WORLD, &status);
-        MPI_Send(recieved, strlen(recieved), MPI_CHAR, 0, 99, MPI_COMM_WORLD);
+        MPI_Recv(recieved, 20, MPI_CHAR, world_rank--, 99, MPI_COMM_WORLD, &status);
+        MPI_Send(recieved, strlen(recieved), MPI_CHAR, partner_Process, 99, MPI_COMM_WORLD);
     }
 
     // Finalize the MPI environment. No more MPI calls can be made after this
