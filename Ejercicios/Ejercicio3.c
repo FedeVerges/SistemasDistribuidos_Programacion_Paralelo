84bbf57dbffd8e26f7d2321de2f92b7cdc9a06bc
fefo <fede_16_98@hotmail.com>
2020-05-03 18:26:17 -0300

    -

--- Ejercicios/Ejercicio3.c
+++ Ejercicios/Ejercicio3.c
@@ -38,54 +38,46 @@ int main(int argc, char **argv)
     int indice = 0;
     int vector[6]; // buffer
     int cant_columns = 6;
-    int *recvRow = (int *) malloc (sizeof(int)* cant_columns);
     int cant_rows = 6;
+    int total_values = cant_columns * cant_rows;
 
+    int *recvRow = (int *) malloc (sizeof(int)* cant_columns); // create the buffer.
+    int *matrix = (int *) malloc (sizeof(int)* total_values); // create the matriz buffer.
+    int *vector  = (int *) malloc (sizeof(int)* cant_columns);
     if (world_rank == 0)
     {
-        int recvRow[6];
-        int vector[6]; // buffer
-        int cant_columns = 6;
-        int cant_rows = 6;
-        int total_values = cant_columns * cant_rows;
+        // // Generate Data
+        // int cant_rows_to_send = 5; // The process 0 multiply the first row.
+        // int columns_send [cant_rows_to_send];
 
-        // Generate Data
-        int cant_rows_to_send = 5; // The process 0 multiply the first row.
-        int columns_send [cant_rows_to_send];
-
-        int matrix[6][6] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 34, 35, 36}};
+        //int matrix[6][6] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 34, 35, 36}};
         
-        int values [36] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25, 26, 27, 28, 29, 30,31, 32, 33, 34, 35, 36};
+        // *(matrix) [36] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25, 26, 27, 28, 29, 30,31, 32, 33, 34, 35, 36};
         
-        int array_to_Send [total_values - cant_columns];
+        // int array_to_Send [total_values - cant_columns];
         
-        int row0 [cant_columns]; // the First row is processed by the process 0.
+        // int row0 [cant_columns]; // the First row is processed by the process 0.
         
-        for (int i=0; i<cant_columns; i+=1){
-            vector[i] =i;
-            row0[i] = values[i];
+        // for (int i=0; i<cant_columns; i+=1){
+        //     vector[i] =i;
+        //     row0[i] = values[i];
+        // }
+        
+        for(int i=1; i<=total_values; i+=1){ // Generate values.
+            matrix[i] = i;
         }
-        
-        for(int i=0; i< total_values - cant_columns; i+=1){
-            array_to_Send[i] = values[i + cant_columns];
+        for(int i=1; i<= cant_columns; i+=1){ // generate Vector
+            vector[i]=i;
         }
-
-        MPI_Scatter(array_to_Send, 6, MPI_INT, recvRow, 6, MPI_INT, 0, MPI_COMM_WORLD); // send the colums to each process.
-        
-        MPI_Bcast(vector,6,MPI_INT,0,MPI_COMM_WORLD);
-
-        int final_result = mulitMatrix(row0, vector, cant_columns);
 
     }
-    else
-    {
-        for (int i=0; i<cant_columns; i+=1){
+    MPI_Scatter(matrix, 6, MPI_INT, recvRow, 6, MPI_INT, 0, MPI_COMM_WORLD); // send the colums to each process.
+    MPI_Bcast(vector,6,MPI_INT,0,MPI_COMM_WORLD);
 
-           printf("La fila de la matriz es %d \n\n", recvRow[i]);
-           printf("El vector enviado es %d \n\n", vector[i]);
+    printf("valor del primer elemento del vector %d ", vector[0]);
+    printf("soy el proceso %d y mi valor de la primer fila  %d ", world_rank,recvRow[0]);
+    
 
-        }
-    }
 
     // Finalize the MPI environment. No more MPI calls can be made after this
     MPI_Finalize();
