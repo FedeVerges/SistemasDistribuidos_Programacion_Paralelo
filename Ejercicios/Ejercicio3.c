3d94e95864bba19e007c2bea8fc69c01a7ee5af0
fefo <fede_16_98@hotmail.com>
2020-05-03 17:09:59 -0300

    ex 3 details

--- Ejercicios/Ejercicio3.c
+++ Ejercicios/Ejercicio3.c
@@ -42,29 +42,34 @@ int main(int argc, char **argv)
 
     if (world_rank == 0)
     {
+        int recvRow[6];
+        int indice = 0;
+        int vector[6]; // buffer
+        int cant_columns = 6;
+        int cant_rows = 6;
+
         // Generate Data
-        
         int cant_rows_to_send = 5; // The process 0 multiply the first row.
         int columns_send [cant_rows_to_send];
 
         int matrix[6][6] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 34, 35, 36}};
         int row0 [cant_columns];
         
-        for (int i=0; i<cant_columns; i+1){
+        for (int i=0; i<cant_columns; i+=1){
             vector[i] =i;
             row0[i] = matrix[0][i];    
         }
 
-        for (int i=0; i<cant_columns; i+1){
+        for (int i=0; i<cant_columns; i+=1){
 
            printf("La fila del proceso 0 es %d \n\n", row0[i]);
            printf("El vector enviado es %d \n\n", vector[i]);
 
         }
        
-        for(int i=0; i<cant_columns; i+1){ // number of columns.
+        for(int i=0; i<cant_columns; i+=1){ // number of columns.
 
-            for(int f=0; f<cant_rows_to_send; f+1){ // number of rows
+            for(int f=0; f<cant_rows_to_send; f+=1){ // number of rows
                 columns_send[f]= matrix[f+1][i];
                 
             }
@@ -73,7 +78,8 @@ int main(int argc, char **argv)
 
         }
         MPI_Bcast(vector,6,MPI_INT,0,MPI_COMM_WORLD);
-        int final_result = mulitMatrix(row0, vector);
+
+        int final_result = mulitMatrix(row0, vector, cant_columns);
 
     }
     else
@@ -90,10 +96,9 @@ int main(int argc, char **argv)
     MPI_Finalize();
 }
 
-int mulitMatrix (int row[], int vector[]){
-    int cant = sizeof(row);
-    int result;
-    for (int i =0; i<cant; i+1){
+int mulitMatrix (int row[], int vector[], int size){
+    int result = 0;
+    for (int i =0; i<size; i+=1){
         result += row[i] * vector[i];
     }
     return result;
