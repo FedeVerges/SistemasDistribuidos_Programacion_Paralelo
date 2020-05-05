987e5280d28b9ce474da0782a506ee913ca1edcd
fefo <fede_16_98@hotmail.com>
2020-05-05 10:21:54 -0300

    -

--- Ejercicios/Ejercicio3.c
+++ Ejercicios/Ejercicio3.c
@@ -5,8 +5,8 @@
 
 int main(int argc, char **argv)
 {
-    int cant_columns = 0;
-    int cant_rows = 0;
+    int cant_columns = 6;
+    int cant_rows = 6;
     int total_values =  cant_columns * cant_rows;
     // Initialize the MPI environment. The two arguments to MPI Init are not
     // currently used by MPI implementations, but are there in case future
