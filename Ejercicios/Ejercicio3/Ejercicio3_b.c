12a7661769cbe74692bddead590c2ab2cce404bb
fefo <fede_16_98@hotmail.com>
2020-05-30 19:46:44 -0300

    Excercise 3 complete

--- Ejercicios/Ejercicio3/Ejercicio3_b.c
+++ Ejercicios/Ejercicio3/Ejercicio3_b.c
@@ -5,22 +5,45 @@
 
 void getSubMatrix(int matrix[], int submatrix[], int start, int finish)
 {
-    // Eliminar las filas compartidas de la matriz.
+    // get the matri'x part to send.
     for (int i = start; i < finish; i += 1)
     {
         submatrix[i - start] = matrix[i];
     }
 }
 
+int mulitMatrix(int row[], int vector[], int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; i += 1)
+    {
+        result += row[i] * vector[i];
+    }
+    return result;
+}
+
+
 int main(int argc, char **argv)
 {
-    int cant_columns = 6;
-    int cant_rows = 6;
+    int cant_columns = 5; // cant_columns = row lenght
+    int cant_rows = 5;
     int total_values = cant_columns * cant_rows;
+
+    int no_repeat = 0;   // To Print correctly the vector.
+    int indexUnique = 0; // Index for print once the result vector
+
+    int result_vector[cant_columns]; // Result Vector.
+
+    if (argc != 2)
+    {
+        printf("Cantidad de argumentos erronea");
+        exit(1);
+    }
+
     // Initialize the MPI environment. The two arguments to MPI Init are not
     // currently used by MPI implementations, but are there in case future
     // implementations might need the arguments.
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
     MPI_Status status;
 
     //Get the number of processes
@@ -38,83 +61,64 @@ int main(int argc, char **argv)
 
     MPI_Get_processor_name(processor_name, &name_len);
 
-    int recvRow[cant_columns];
-    int matrix[total_values];
-    int vector[cant_columns];
-    int result_vector[cant_columns];
-
+    int recvRow[cant_columns]; // Shared array to send the values of matri'x columns.
+    int matrix[total_values];  // Matrix
+    int vector[cant_columns];  // Vector
 
     if (world_rank == 0)
     {
+
         for (int i = 0; i < total_values; i += 1)
-        {                      // Generate values.
-            matrix[i] = i + 1; // generando el vector con las filas de la matriz
+        {
+            // Generate values.
+            // Generando el vector con las filas de la matriz
+            matrix[i] = i + 1;
         }
         for (int i = 0; i < cant_columns; i += 1)
-        { // generate Vector
+        {
+            // Generate Vector
             vector[i] = i + 1;
         }
     }
 
-    // asumir que la info es enviada a todos los procesos
+    int cant_elements_to_send = cant_columns * world_size; // amount elements to send on Scatter.
+    int submatrix[cant_elements_to_send];                  // Array to keep the part of the matrix to send on Scatter.
 
-    // otra opcion.
-    // En el caso de 6 filas, 3 procesos, 2 filas por proceso.
+    int result; // Final result of the multiplication.
 
-    if (cant_rows % world_size == 0)
-    {                                                          // Caso de filas multiplo de procesos.
-        int rows_for_process = cant_rows / world_size;         // cuantas filas le corresponden a cada proceso.
-        int cant_elements_to_send = cant_columns * world_size; // cantidad de elementos por tanda a enviar.
-        int submatrix[cant_elements_to_send];
+    MPI_Bcast(&vector, cant_columns, MPI_INT, 0, MPI_COMM_WORLD); // Share the vector to each process.
 
-        int final_result[rows_for_process]; // vector resultado.
-        
-        MPI_Bcast(&vector, cant_columns, MPI_INT, 0, MPI_COMM_WORLD); // Share the vector to each process.
-
-        for (int i = 0; i < rows_for_process; i += 1)
-        {
-
-            getSubMatrix(matrix, submatrix, (cant_elements_to_send * i), cant_elements_to_send * (i+1)); // dividimos la matriz
-
-            MPI_Scatter(submatrix, cant_columns, MPI_INT, recvRow, cant_columns, MPI_INT, 0, MPI_COMM_WORLD); // compartos las filas para cada proceso.
-
-            final_result[i]=mulitMatrix(recvRow, vector, cant_columns); 
-            
-            MPI_Gather(&final_result[i], 1, MPI_INT, result_vector, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        
-
-
-        }
-    }
-
-
-    //  // tomar como un arreglo, hacer un for.
-    
-
-    // PENSAR EN FORMA DE IR DIVIDIENDO LAS FILAS ENTRE LOS PROCESOS.
-
-    // 
-    // Como almaceno lso resultados!!.
-
-    // if (world_rank == 0)
-    // {
-    //     printf("el resultado de la operacion es ( ");
-    //     for (int i = 0; i < cant_rows; i += 1)
-    //     {
-    //         printf(" %d ", result_vector[i]);
-    //     }
-    //     printf(" ). ");
-    // }
-    // // Finalize the MPI environment. No more MPI calls can be made after this
-    MPI_Finalize();
-}
-
-int mulitMatrix(int row[], int vector[], int size)
-{
-    int result = 0;
-    for (int i = 0; i < size; i += 1)
+    for (int i = 0; i < total_values; i += cant_elements_to_send)
     {
-        result += row[i] * vector[i];
+        getSubMatrix(matrix, submatrix, i, i + cant_elements_to_send);
+
+        MPI_Scatter(submatrix, cant_columns, MPI_INT, recvRow, cant_columns, MPI_INT, 0, MPI_COMM_WORLD); // share the rows of the matrix.
+
+        result = mulitMatrix(recvRow, vector, cant_columns); 
+
+        MPI_Gather(&result, 1, MPI_INT, result_vector, 1, MPI_INT, 0, MPI_COMM_WORLD); //  The Process 0 collect the data from the other process.
+
+
+        // Printing Values.
+        if (world_rank == 0)
+        {
+            if (no_repeat == 0)
+            {
+                printf("Vector Resultado : ");
+            }
+            for (int i = 0; i < world_size; i += 1)
+            {
+                if (indexUnique < cant_columns)
+                {
+                    printf(" %d ", result_vector[i]);
+                }
+                indexUnique++;
+            }
+
+            no_repeat += 1;
+        }
+        result = 0;
     }
-    return result;
+
+    MPI_Finalize();
 }
